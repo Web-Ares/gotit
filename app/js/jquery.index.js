5c6ad57ed7d40490d07d1d7905ca68c0594f6acf
Scorpionsc <petruninnikolay@gmail.com>
2016-10-27 12:33:34 +0300

    team

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -3,12 +3,6 @@
 
     $( function() {
 
-        $.each( $('.squad__items'), function () {
-
-            new SquadSlider( $(this) );
-
-        } );
-
         $.each( $('.our-clients__items'), function () {
 
             new ClientsSlider( $(this) );
@@ -18,117 +12,6 @@
 
     } );
 
-    var SquadSlider = function ( obj ) {
-
-        var _self = this,
-            _obj = obj,
-            _window = $( window ),
-            _swiperInit = false,
-            _swiper,
-            _squad = $('.squad'),
-            _squadDisk = $('.squad__disk');
-
-        var _addEvents = function () {
-
-                _window.on( {
-                    load: function() {
-
-                        if( _window.width() >= 768 ) {
-
-                            _setDiskSize();
-
-                        }
-
-
-                    },
-                    resize: function() {
-
-                        if( _window.width() < 768 ) {
-
-                            if( !_swiperInit ) {
-
-                                _initSwiper();
-                                _swiperInit = true;
-
-                            }
-
-                            _squadDisk.attr('style','')
-
-
-                        } else {
-
-                            if( _swiperInit ) {
-
-                                _destroySwiper();
-                                _swiperInit = false;
-                            }
-
-                            _setDiskSize();
-
-                        }
-
-
-                    }
-                } );
-
-            },
-            _initSwiper = function() {
-
-                _swiper = new Swiper( _obj.find( '.swiper-container' ), {
-                    slidesPerView: 1.75,
-                    centeredSlides: true
-                } );
-
-            },
-            _destroySwiper = function() {
-
-                _swiper.destroy( true, true);
-
-            },
-            _setDiskSize = function() {
-
-                var height = _squad.innerHeight(),
-                    width = _squad.innerWidth(),
-                    newSize;
-
-                if( height > width ) {
-
-                    newSize  = width
-
-                } else {
-
-                    newSize  = height
-
-                }
-
-                _squadDisk.width( newSize );
-                _squadDisk.height( newSize );
-                _squadDisk.css( {
-
-                    'margin-top': '-' +newSize/2+ 'px',
-                    'margin-left': '-' +newSize/2+ 'px'
-
-                } );
-
-            },
-            _init = function() {
-                _obj[0].obj = _self;
-                _addEvents();
-
-                if( _window.width() < 768 ) {
-
-                    if( !_swiperInit ) {
-
-                        _initSwiper();
-                        _swiperInit = true;
-                    }
-
-                }
-
-            };
-
-        _init();
-    };
 
     var ClientsSlider = function ( obj ) {
 
--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -54,6 +54,12 @@
 
         } );
 
+        $.each( $('.squad__items'), function () {
+
+            new SquadSlider( $(this) );
+
+        } );
+
 
     } );
 
@@ -1454,6 +1460,168 @@
             _init = function () {
                 _addEvents();
                 _addPictures();
+            };
+
+        _init();
+    };
+
+    var SquadSlider = function ( obj ) {
+
+        var _self = this,
+            _obj = obj,
+            _window = $( window ),
+            _swiperInit = false,
+            _swiper,
+            _squad = $('.squad'),
+            _squadDisk = $('.squad__disk');
+
+        var _addEvents = function () {
+
+                _window.on( {
+                    load: function() {
+
+                        if( _window.width() >= 768 ) {
+
+                            _setDiskSize();
+
+                        }
+
+
+                    },
+                    resize: function() {
+
+                        if( _window.width() < 768 ) {
+
+                            if( !_swiperInit ) {
+
+                                _initSwiper();
+                                _swiperInit = true;
+
+                            }
+
+                            _squadDisk.attr('style','')
+
+
+                        } else {
+
+                            if( _swiperInit ) {
+
+                                _destroySwiper();
+                                _swiperInit = false;
+                            }
+
+                            _setDiskSize();
+
+                        }
+
+
+                    }
+                } );
+
+            },
+            _initSwiper = function() {
+
+                _swiper = new Swiper( _obj.find( '.swiper-container' ), {
+                    slidesPerView: 1.75,
+                    centeredSlides: true
+                } );
+
+            },
+            _destroySwiper = function() {
+
+                _swiper.destroy( true, true);
+
+            },
+            _setDiskSize = function() {
+
+                var height = _squad.innerHeight(),
+                    width = _squad.innerWidth(),
+                    newSize;
+
+                if( height > width ) {
+
+                    newSize  = width
+
+                } else {
+
+                    newSize  = height
+
+                }
+
+                _squadDisk.width( newSize );
+                _squadDisk.height( newSize );
+                _squadDisk.css( {
+
+                    'margin-top': '-' + newSize/2 + 'px',
+                    'margin-left': '-' + newSize/2 + 'px'
+
+                } );
+
+
+                if( _obj.parents().hasClass('team') ) {
+
+                    if ( newSize < _squadDisk.css('max-height') ) {
+
+
+
+                        _squadDisk.width( newSize );
+                        _squadDisk.height( newSize );
+                        _squadDisk.css( {
+
+                            'margin-top': '-' + newSize/2 + 'px',
+                            'margin-left': '-' + newSize/2 + 'px'
+
+                        } );
+
+                    } else {
+
+                        _squadDisk.width( _squadDisk.css('max-height') );
+                        _squadDisk.height( _squadDisk.css('max-height') );
+                        _squadDisk.css( {
+
+                            'margin-top': '-' + parseInt( _squadDisk.css('max-height'))/2  + 'px',
+                            'margin-left': '-' + parseInt( _squadDisk.css('max-height'))/2  + 'px'
+
+                        } );
+
+                        console.log(_squadDisk.css('max-height'))
+                        console.log(parseInt( _squadDisk.css('max-height') ))
+
+                    }
+
+                    if( _squadDisk.innerHeight() < $('.squad__inner').innerHeight() ) {
+
+                        _squadDisk.css( {
+
+                            top: 0,
+                            'margin-top': 0,
+                            'margin-left': '-' + parseInt( _squadDisk.css('max-height') )/2 + 'px'
+
+                        } );
+
+                    }
+
+
+
+                }
+
+
+
+            },
+            _init = function() {
+                _obj[0].obj = _self;
+                _addEvents();
+
+                if( _window.width() < 768 ) {
+
+                    if( !_swiperInit ) {
+
+                        _initSwiper();
+                        _swiperInit = true;
+                    }
+
+                }
+
             };
 
         _init();
